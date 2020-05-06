065cad75c6de680184332c733ffeff173ea35b8e
Benjamin-Lea <48414452+Benjamin-Lea@users.noreply.github.com>
2020-05-06 14:30:56 -0700

    Add files via upload

--- Application.py
+++ Application.py
@@ -2,46 +2,81 @@ from flask import  Flask, redirect, url_for, render_template, request
 import py_functions as fc
 app = Flask(__name__)
 ##variables##    
-input_variables = ['A0','B0','C0',
-                   'A1','B1','C1',
-                   'A2','B2','C2',
-                   'A3','B3','C3',
-                   'A4','B4','C4',
-                   'A5','B5','C5',
-                   'A6','B6','C6',
-                   'A7','B7','C7']
+input_variables = [ 'A0','B0','C0','D0',
+                    'A1','B1','C1','D1',
+                    'A2','B2','C2','D2',
+                    'A3','B3','C3','D3',
+                    'A4','B4','C4','D4',
+                    'A5','B5','C5','D5',
+                    'A6','B6','C6','D6',
+                    'A7','B7','C7','D7',
+                    'A8','B8','C8','D8',
+                    'A9','B9','C9','D9',
+                    'A10','B10','C10','D10',
+                    'A11','B11','C11','D11',
+                    'A12','B12','C12','D12',
+                    'A13','B13','C13','D13',
+                    'A14','B14','C14','D14',
+                    'A15','B15','C15','D15']
 
 user_input = [None] *len(input_variables)
-binary = [0,0,0,
-          0,0,1,
-          0,1,0,
-          0,1,1,
-          1,0,0,
-          1,0,1,
-          1,1,0,
-          1,1,1] 
-
-
+#user_input = []
+binary = [0,0,0,0,
+          0,0,0,1,
+          0,0,1,0,
+          0,0,1,1,
+          0,1,0,0,
+          0,1,0,1,
+          0,1,1,0,
+          0,1,1,1,
+          1,0,0,0,
+          1,0,0,1,
+          1,0,1,0,
+          1,0,1,1,
+          1,1,0,0,
+          1,1,0,1,
+          1,1,1,0,
+          1,1,1,1] 
+num = 3
 @app.route("/")
 def index():
     return render_template("index.html")
-
 @app.route("/Userinput", methods=["POST","GET"])
-def login():
+def Userinput():
     if request.method == "POST":
-    	for i in range(len(input_variables)):
-    		user_input[i]=(request.form[input_variables[i]])
-    	T_data = fc.T_compair(user_input)
+    	for i in range(2**num):
+            for j in range(num):
+                user_input[4*i+j]=(request.form[input_variables[4*i+j]])
+                #user_input.append(request.form[input_variables[4*i+j]])
+    	T_data = fc.T_compair(user_input,binary,num)
     	T_data = ''.join(str(i) for i in T_data)
     	next_state = ''.join(str(i) for i in user_input)
-    	return redirect(url_for("Output", T_data = T_data, next_state=next_state))
+    	return redirect(url_for("T_Output",T_data = T_data,next_state=next_state))
     else:
-    	return render_template("Userinput.html", input_variables=input_variables , bin = binary)
+    	return render_template("Userinput.html",input_variables=input_variables,bin=binary)
 
-@app.route("/Output/<T_data>/<next_state>")
-def Output(T_data,next_state): 
-    eqA= fc.Eq(T_data,0)#Pass 0 for A , 1 for B and 2 for C
-    return render_template('OutPutBase.html', T_data = T_data, next_state=next_state,bin = binary,eqA=eqA)
+@app.route("/T_Output/<T_data>/<next_state>")
+def T_Output(T_data,next_state): 
+    #eqA= fc.Eq(T_data,0,binary,num)#Pass 0 for A , 1 for B and 2 for C
+    #eqB= fc.Eq(T_data,1,binary,num)
+    #eqC= fc.Eq(T_data,2,binary,num)
+    #eqD= fc.Eq(T_data,3,binary,num)
+    return render_template('OutPutBase.html', T_data = T_data,
+     next_state=next_state,bin=binary,eqA=1,eqB=2,eqC=3)
 
+@app.route("/JK_Userinput", methods=["POST","GET"])
+def JK_Userinput():
+    if request.method == "POST":
+        for i in range(len(input_variables)):
+            user_input[i]=(request.form[input_variables[i]])
+        JK_data = fc.JK_compair(user_input) #
+        JK_data = ''.join(str(i) for i in JK_data)
+        next_state = ''.join(str(i) for i in user_input)
+        return redirect(url_for("JK_Output", JK_data = JK_data, next_state=next_state))
+    else:
+        return render_template("Userinput.html", input_variables=input_variables , bin = binary)
 
-
+@app.route("/JK_Output/<JK_data>/<next_state>")
+def JK_Output(JK_data,next_state):
+    return render_template('JK_Output.html',JK_data = JK_data,
+     next_state=next_state,bin=binary)
---
+++ py_functions.py
@@ -0,0 +1,72 @@
+from operator import xor
+def T_compair(T_data,binary,num):
+	T_data = list(map(int,T_data))
+	for i in range(2**num):
+		for j in range(num):
+			T_data[i*num+j] = xor(T_data[i*num+j],binary[i*4+j+(4-num)])
+	T_data = ''.join(str(i) for i in T_data)
+	print(T_data)
+	return T_data 
+
+def Eq(Data,ABC,binary,num):#Pass 0 for A , 1 for B and 2 for C
+  Data = list(map(int, str(Data))) 
+  eq =[]
+  if(ABC == 0):
+    a=0
+  if(ABC == 1):
+    a=-1
+  if(ABC == 2):
+    a=-2
+  if(ABC == 3):
+    a=-3
+  for o in range(len(binary)):
+    if o%4==ABC:
+      c=a
+      if Data[o]==1:
+        for i in range(num):
+          if i==0:
+            if(len(eq)>0):
+              eq.append(" + ")
+            if binary[o+c]==1:
+              eq.append("(A")
+            else:
+              eq.append("(~A")
+            c=c+1
+          elif i==1:
+            if binary[o+c]==1:
+              eq.append("B")
+            else:
+              eq.append("~B")
+            c=c+1
+          elif i==2:
+            if binary[o+c]==1:
+              eq.append("C)")
+            else:
+              eq.append("~C")
+            c=c+1
+          elif i==3:
+            if binary[o+c]==1:
+              eq.append("D")
+            else:
+              eq.append("~D")
+          eq.append(")")
+  eq= ''.join(str(i) for i in eq)
+  if eq == '':
+    return "0"
+  return eq
+
+def JK_compair(jk_data):
+	binary = [0,0,0,0,0,1,0,1,0,0,1,1,1,0,0,1,0,1,1,1,0,1,1,1] 
+	jk = []
+	for o in range(len(binary)):
+		if(binary[o]==1):
+			jk.append("X")
+		else:
+			jk.append(str(jk_data[o]))
+	for o in range(len(binary)):
+		if(binary[o]==0):
+			jk.append("X")
+		else:
+			jk.append(str(jk_data[o]))
+	jk= ''.join(str(i) for i in jk)
+	return jk
